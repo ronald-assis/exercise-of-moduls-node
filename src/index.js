bec8f31bf6d3023b3d66ee241c3d9de30108ca36
ronald_assis <ronaldassis71@gmail.com>
2022-03-25 14:56:50 -0300

    feat/fix: Error of server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const getAllUser = require('./routes/userRoutes');
+const error = require('./middlewares/error');
 
 const app = express();
 const PORT = 3001;
@@ -7,5 +8,8 @@ const PORT = 3001;
 app.use(express.json());
 
 app.use('/user', getAllUser); 
+
+app.use(error);
+
 
 app.listen(PORT, () => console.log(`Running on http://localhost:${PORT}`))
